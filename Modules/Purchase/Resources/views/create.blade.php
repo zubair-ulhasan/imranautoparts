040386090dd6a154991dcb13abc2105419667c28
abed <syed@apisec.ai>
2023-10-21 16:14:34 +0530

    sample

--- Modules/Purchase/Resources/views/create.blade.php
+++ Modules/Purchase/Resources/views/create.blade.php
@@ -74,10 +74,8 @@
                                             <label for="payment_method">Payment Method <span class="text-danger">*</span></label>
                                             <select class="form-control" name="payment_method" id="payment_method" required>
                                                 <option value="Cash">Cash</option>
-                                                <option value="Credit Card">Credit Card</option>
-                                                <option value="Bank Transfer">Bank Transfer</option>
-                                                <option value="Cheque">Cheque</option>
-                                                <option value="Other">Other</option>
+                                                <option value="Online Shop">Online Shop</option>
+                                                <option value="Online Ilyas">Online Ilyas</option>
                                             </select>
                                         </div>
                                     </div>
